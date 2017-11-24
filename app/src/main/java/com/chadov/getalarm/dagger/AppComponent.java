98594d06042868386eba02cad8c3a0842ccc4337
timusen <timchadov@gmail.com>
2017-11-24 15:26:01 +0300

    Dagger component added

---
+++ app/src/main/java/com/chadov/getalarm/dagger/AppComponent.java
@@ -0,0 +1,15 @@
+package com.chadov.getalarm.dagger;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by ChadovTA on 24.11.2017.
+ */
+
+@Singleton
+@Component(modules = {AppModule.class})
+public interface AppComponent {
+
+}
---
+++ app/src/main/java/com/chadov/getalarm/dagger/AppModule.java
@@ -0,0 +1,28 @@
+package com.chadov.getalarm.dagger;
+
+import android.app.Application;
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by ChadovTA on 24.11.2017.
+ */
+
+@Module
+public class AppModule {
+    private Application application;
+
+    public AppModule(Application application) {
+        this.application = application;
+    }
+
+    @Provides
+    @Singleton
+    public Context provideContext() {
+        return application;
+    }
+}
